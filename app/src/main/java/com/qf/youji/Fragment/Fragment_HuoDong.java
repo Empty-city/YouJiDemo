8bf2e525a6679f0558ba1d2b8bc80bd5cb94c4e3
FlyToLeft <765985389@qq.com>
2016-07-07 09:29:43 +0800

    数据解析活动界面

--- app/src/main/java/com/qf/youji/Fragment/Fragment_HuoDong.java
+++ app/src/main/java/com/qf/youji/Fragment/Fragment_HuoDong.java
@@ -9,7 +9,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.qf.youji.R;
+import com.qf.youji.eneity.DataBean;
 import com.qf.youji.eneity.HuoDong;
+
+import it.sephiroth.android.library.picasso.Picasso;
 
 /**
  * 活动选项对应的Fragment
@@ -23,16 +26,14 @@ public class Fragment_HuoDong extends Fragment {
     private TextView tv_title_id;//标题
     private TextView tv_address_id;//地址
     private TextView tv_people_count_id;//报名人数
-    private HuoDong hd;//活动类的实例
-    private HuoDong.DataBean db;//静态内部类
+    private DataBean db;//类的对象
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        hd = new HuoDong();
 
-        db = new HuoDong.DataBean();
+        db = new DataBean();
     }
 
     @Override
@@ -55,7 +56,7 @@ public class Fragment_HuoDong extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         //设置图片
-//        Picasso.with(getContext()).load(db.getFace()).into(iv_big_id);
+        Picasso.with(getContext()).load(db.getFace().toString()).into(iv_big_id);
 
         //设置时间
         tv_time_id.setText(db.getShow_time());
@@ -63,7 +64,7 @@ public class Fragment_HuoDong extends Fragment {
         tv_price_id.setText(db.getPrice());
         //设置标题
         tv_title_id.setText(db.getTitle());
-        //设置副标题或者是地址
+        //设地址
         tv_address_id.setText(db.getShow_tip());
 
         //设置报名人数
