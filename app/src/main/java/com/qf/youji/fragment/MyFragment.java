168a9b615d37cd9dcf18978f180a15cfd38813b3
Empty-city <ahl4251973ys>
2016-07-07 09:04:15 +0800

    早上Fragment各个模块构建

---
+++ app/src/main/java/com/qf/youji/fragment/MyFragment.java
@@ -0,0 +1,28 @@
+package com.qf.youji.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2016/7/7.
+ */
+public class MyFragment extends android.support.v4.app.Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
